927d78acf26d58500cdc217693588c7df0ad43e0
Andy Kwok <andy.kwok@improving.com>
2025-12-30 11:09:49 -0800

    Example: Neptune and Neptune Analytics configuration reference for Mem0 (#299)
    * Update mem0 example
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Update doc
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    * Minimise diff
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    ---------
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- docs/examples/python/memory_agent.py
+++ docs/examples/python/memory_agent.py
@@ -66,9 +66,14 @@ load_dotenv()
 # os.environ['OPENSEARCH_HOST'] = "your-opensearch-host.us-west-2.aoss.amazonaws.com"
 # os.environ['AWS_ACCESS_KEY_ID'] = "your-aws-access-key-id"
 # os.environ['AWS_SECRET_ACCESS_KEY'] = "your-aws-secret-access-key"
-# Setup Neptune Analytics vector backend
-# os.environ['NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER_VECTOR'] = "g-sample-graph-id"
-# Setup optional Neptune Analytics graph backend
+
+# For graph memory with Neptune, see:
+# https://docs.mem0.ai/open-source/features/graph-memory
+# Two options are available for Neptune integration:
+
+# Setup option 1: Neptune Database as graph backend
+# os.environ['NEPTUNE_DATABASE_ENDPOINT'] = "your-neptune-host.us-west-2.neptune.amazonaws.com"
+# Setup option 2: Neptune Analytics as vector and graph backend
 # os.environ['NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER'] = "g-sample-graph-id"
 USER_ID = "mem0_user"
 
